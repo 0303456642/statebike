9be616abb6a1a046c3da37a5efe1fbafc94ae1fb
mel <mel@debianitx>
2015-11-04 01:22:56 -0300

    all except loan

--- StateBike/Sbike/models.py
+++ StateBike/Sbike/models.py
@@ -2,35 +2,30 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
 
+
 class SBikeUser(models.Model):
     user = models.OneToOneField(User)
 
     dni = models.IntegerField(blank=False, primary_key=True)
     phone_number = models.IntegerField(null=True, blank=True)
 
-    def  __str__(self):
+    def __str__(self):
         return str(self.dni)
+
 
 class Client(SBikeUser):
     card_number = models.IntegerField(blank=False, null=True)
     expiration_date = models.DateField(blank=False, null=True)
     security_code = models.IntegerField(blank=False, null=True)
-    def findStation(self):
-        pass
-    def checkPenalty(self):
-        pass
+
 
 class Admin(SBikeUser):
-    def moveBikes(self):
-        pass
-    def createStation(self):
-        pass
-    def createBike(self):
-        pass
+    pass
+
 
 class Employee(SBikeUser):
-    def repairBike(self):
-        pass
+    pass
+
 
 class Station(models.Model):
     employee = models.ForeignKey(Employee)
@@ -40,43 +35,55 @@ class Station(models.Model):
     stock = models.IntegerField(blank=False)
     capacity = models.IntegerField(blank=False)
 
-    def  __str__(self):
+    def __str__(self):
         return str(self.name)
-    def removeFromStock(self):
-        pass
-    def addToStock(self):
-        pass
-    def bikesToRepair(self):
-        pass
+
+    def remove_from_stock(self):
+        self.stock = self.stock - 1
+
+    def add_to_stock(self):
+        self.stock = self.stock + 1
+
 
 class Bike(models.Model):
     AVAILABLE = 'AV'
     TAKEN = 'TK'
     BROKEN = 'BR'
-    
+
     STATE_CHOICES = (
         (AVAILABLE, 'Available'),
         (TAKEN, 'Taken'),
         (BROKEN, 'Broken'),
     )
 
-    state = models.CharField(max_length=2, choices=STATE_CHOICES, default=AVAILABLE)
+    state = models.CharField(
+        max_length=2, choices=STATE_CHOICES,
+        default=AVAILABLE)
+
     station = models.ForeignKey(Station)
-    def  __str__(self):
+
+    def __str__(self):
         return str(self.id)
+
     def take(self):
-        pass
+        self.state = 'TK'
+
     def repair(self):
-        pass
-    def giveBack(self):
-        pass
+        self.state = 'AV'
+
+    def give_back(self):
+        self.state = 'AV'
+
 
 class Loan(models.Model):
     client = models.OneToOneField(Client)
     bike = models.OneToOneField(Bike)
     startDate = models.DateTimeField(default=datetime.now)
-    endDate = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
-    def  __str__(self):
+    endDate = models.DateTimeField(
+        auto_now=False, auto_now_add=False, null=True, blank=True)
+
+    def __str__(self):
         return "Loan: " + str(self.id)
-    def evalPenalty(self):
+
+    def eval_penalty(self):
         pass
