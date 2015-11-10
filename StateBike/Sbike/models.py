594f44ef6cad3fb773db624dda163dbecf6c3cc4
mel <mel.bonias@gmail.com>
2015-11-10 16:09:47 -0300

    sanction v1.0

--- StateBike/Sbike/models.py
+++ StateBike/Sbike/models.py
@@ -18,6 +18,11 @@ class Client(SBikeUser):
     expiration_date = models.DateField(blank=False, null=True)
     security_code = models.IntegerField(blank=False, null=True)
 
+    def edit_card(self, card_number, expiration_date, security_code):
+        self.card_number = card_number
+        self.expiration_date = expiration_date
+        self.security_code = security_code
+        self.save()
 
 class Admin(SBikeUser):
     pass
@@ -90,5 +95,33 @@ class Loan(models.Model):
     def __str__(self):
         return "Loan: " + str(self.id)
 
+    def create_loan(self,):
+        self.client = client
+        self.bike = bike
+        self.save()
+
     def eval_sanction(self):
         pass
+
+
+class Sanction(models.Model):
+    loan = models.OneToOneField(Loan)
+    amount = models.DecimalField(max_digits=, decimal_places=2, null=True, blank=True)
+    under = models.CharField(max_length=100)
+    date = models.DateTimeField(null=True, blank=True)
+    deposition = models.TextField(null=True, blank=True)
+
+    def __set_under(self, under):
+        self.under = under
+        self.save()
+
+    def create_sanction(self, loan, amount, under, date):
+        self.__set_under(under)
+        self.loan = loan
+        self.amount = amount
+        self.date = date
+        self.save()
+
+    def generate_deposition(self, deposition):
+        self.deposition = deposition
+        self.save()
